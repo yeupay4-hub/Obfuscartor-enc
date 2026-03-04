b6b26ab70d19997f3f8978ec87c398c19c186c8e
anhnguyencoder <yeupay4@gmail.com>
2026-03-04 16:09:15 +0700

    enc.py

--- enc.py
+++ enc.py
@@ -7,14 +7,7 @@ def rb2():
     return ''.join(random.choices([chr(i) for i in range(44032, 55204) if chr(i).isprintable() and chr(i).isidentifier()], k=10))
 
 def encunicode():
-    pool = []
-    pool += list(range(0x2160, 0x217F))
-    pool += list(range(0x2C00, 0x2C5F))
-    pool += list(range(0x0180, 0x024F))
-    pool += random.sample(range(0xE000, 0xF8FF), 15)
-
-    random.shuffle(pool)
-    return [chr(x) for x in pool[:16]]
+    return [chr(x) for x in random.sample(range(0x2160, 0x217F), 16)]
 
 meo = encunicode()
 
@@ -30,20 +23,7 @@ if expected not in data:
     raise __builtins__['Version']
 '''
 
-anhambuiltins = checkvs+"""
-import sys
-
-_b = sys.modules['builtins']
-__p = getattr(_b, ''.join(chr(i) for i in (112,114,105,110,116)))
-__e = getattr(_b, ''.join(chr(i) for i in (101,118,97,108)))
-__x = getattr(_b, ''.join(chr(i) for i in (101,120,101,99)))
-for _n in ('print', 'eval', 'exec'):
-    globals().pop(_n, None)
-
-del _b, _n
-"""
-
-antihookvip = anhambuiltins+"""
+antihookvip = checkvs+"""
 class __ngan_hook_builtins__:
     def __init__(self):
         try:
@@ -94,6 +74,19 @@ if str(len) != '<built-in function len>':
     raise fn
 if str(__import__('marshal').loads) != '<built-in function loads>':
     raise fn
+a = __import__('sys')
+b = __import__('builtins')
+t = __import__('types')
+i = __import__('inspect')
+if a.modules.get('builtins') is not b: raise fn
+if not isinstance(b.exec, t.BuiltinFunctionType): raise fn
+if getattr(b.exec, '__module__', '') != 'builtins': raise fn
+if hasattr(b, '__file__'): raise fn
+if id(exec) != id(b.exec): raise fn
+try:
+    i.getsource(b.exec)
+    raise fn
+except (OSError, TypeError):pass
 if 'dis' in globals() and hasattr(dis,'dis') and 'built-in' not in str(dis.dis): raise SystemExit
 with open(__file__,'r',encoding='utf8') as m:m=m.readlines()
 if len(m) != 8:
@@ -102,19 +95,96 @@ if len(m) != 8:
 Anhnguyencoder = vars(globals()['__builtins__'])
 """
 
+anti1 = anti+r"""
+import traceback, marshal
+
+ch = set()
+am = {'builtins', '__main__'}
+
+def vv():
+    raise MemoryError('Anhnguyencoder...') from None
+
+def cb(fn):
+    if callable(fn) and fn.__module__ not in am:
+        ch.add(fn.__module__)
+        vv()
+
+def ba(fn):
+    def hi(*args, **kwargs):
+        if args and args[0] in ch:
+            vv()
+        return fn(*args, **kwargs)
+    return hi
+
+def bh():
+    stack = traceback.extract_stack()
+    for frame in stack[:-2]:
+        if frame.filename != __file__:
+            vv()
+
+def ck(fn, md):
+    if callable(fn) and fn.__module__ != md:
+        ch.add(md)
+        raise ImportError(f'>> Detect [{fn.__name__}] call [{md}] ! <<') from None
+
+def ic(md, nf):
+    module = __import__(md)
+    funcs = nf if isinstance(nf, list) else [nf]
+    [ck(getattr(module, func, None), md) for func in funcs]
+
+def lf(val, xy):
+    return callable(val) and xy and val.__module__ != xy.__name__
+
+def kt(lo):
+    if any(lf(val, xy) for val, xy in lo):
+        vv()
+
+def ct(md, nf):
+    module = __import__(md)
+    func = getattr(module, nf, None)
+    if func is None:
+        vv()
+    tg = type(func)
+    def cf(func):
+        if type(func) != tg:
+            vv()
+    cf(func)
+    return func
+
+def ic_type(md, nf):
+    func = ct(md, nf)
+    ck(func, md)
+
+def nc():
+    __import__('sys').settrace(lambda *args, **keys: None)
+    __import__('sys').modules['marshal'] = None
+    __import__('sys').modules['marshal'] = type(__import__('sys'))('marshal')
+    __import__('sys').modules['marshal'].loads = marshal.loads
+
+def sc():
+    nk = {'marshal': 'loads'}
+    [ic_type(md, nf) for md, nf in nk.items()]
+
+    lo = [(__import__('marshal').loads, marshal)s]
+    kt(lo)
+    nc()
+
+sc()
+bh()
+"""
 loadcode = """
 print(' ' * len('Loading...'), end='\\r')
 print('Loading...', end='\\r')
 """
 
-builtins = ['__import__', 'print', 'all', 'any', 'ascii', 'bin', 'breakpoint', 'callable', 'chr', 'compile', 'delattr', 'dir', 'divmod', 'eval', 'exec', 'format', 'getattr', 'globals', 'hasattr', 'hash', 'hex', 'id', 'input', 'isinstance', 'issubclass', 'iter', 'aiter', 'len', 'locals', 'max', 'min', 'next', 'anext', 'oct', 'ord', 'pow', 'print', 'repr', 'round', 'setattr', 'sorted', 'sum', 'vars', 'None', 'Ellipsis', 'NotImplemented', 'False', 'True', 'bool', 'memoryview', 'bytearray', 'bytes', 'classmethod', 'complex', 'dict', 'enumerate', 'filter', 'float', 'frozenset', 'property', 'int', 'list', 'map', 'object', 'range', 'reversed', 'set', 'slice', 'staticmethod', 'str', 'super', 'tuple', 'type', 'zip''open']
+#builtins = ['__import__', 'print', 'all', 'any', 'ascii', 'bin', 'callable', 'chr', 'compile', 'delattr', 'dir', 'divmod', 'eval', 'exec', 'format', 'getattr', 'globals', 'hasattr', 'id', 'input', 'isinstance', 'issubclass', 'iter', 'aiter', 'len', 'locals', 'max', 'min', 'next', 'anext', 'oct', 'ord', 'pow', 'print', 'repr', 'round', 'setattr', 'sorted', 'sum', 'vars', 'None', 'Ellipsis', 'NotImplemented', 'False', 'True', 'bool', 'memoryview', 'bytearray', 'bytes', 'classmethod', 'complex', 'dict', 'enumerate', 'filter', 'float', 'frozenset', 'property', 'int', 'list', 'map', 'object', 'range', 'reversed', 'set', 'slice', 'staticmethod', 'str', 'super', 'tuple', 'type', 'zip', 'open']
 
-class hide(ast.NodeTransformer):
+#class hide(ast.NodeTransformer):
 
-    def visit_Name(self, node):
-        if node.id in builtins:
-            return ast.Subscript(value=ast.Call(func=ast.Name(id='vars', ctx=ast.Load()), args=[ast.Subscript(value=ast.Call(func=ast.Name(id='globals', ctx=ast.Load()), args=[], keywords=[]), slice=ast.Constant(value='__builtins__'), ctx=ast.Load())], keywords=[]), slice=ast.Constant(value=node.id), ctx=ast.Load())
-        return node
+#    def visit_Name(self, node):
+#        if node.id in builtins:
+#            return ast.Subscript(value=ast.Call(func=ast.Name(id='vars', ctx=ast.Load()), args=[ast.Subscript(value=ast.Call(func=ast.Name(id='globals', ctx=ast.Load()), args=[], keywords=[]), slice=ast.Constant(value='__builtins__'), ctx=ast.Load())], keywords=[]), slice=ast.Constant(value=node.id), ctx=ast.Load())
+#        return node
 
 def joinstr(f):
     if not isinstance(f, ast.JoinedStr):
@@ -167,6 +237,51 @@ class fstring(ast.NodeTransformer):
     def visit_JoinedStr(self, node):
         node = joinstr(node)
         return node
+
+def hidden(name, arg):
+    return Call(func=Subscript(value=Call(func=Name(id='vars', ctx=Load()), args=[Subscript(value=Call(func=Name(id='globals', ctx=Load()), args=[], keywords=[]), slice=Constant(value='__builtins__'), ctx=Load())], keywords=[]), slice=Constant(value=name), ctx=Load()), args=[arg], keywords=[])
+
+def joinstr1(node):
+    if not isinstance(node, ast.JoinedStr):
+        return node
+    parts = []
+    for value in node.values:
+        if isinstance(value, ast.Constant):
+            parts.append(value)
+        elif isinstance(value, ast.FormattedValue):
+            expr = value.value
+            if value.conversion == 115:
+                expr = Call(func=Name(id='__AnhNguyen__', ctx=Load()), args=[expr], keywords=[])
+            elif value.conversion == 114:
+                expr = hidden('repr', expr)
+            elif value.conversion == 97:
+                expr = hidden('ascii', expr)
+            else:
+                expr = Call(func=Name(id='__AnhNguyen__', ctx=Load()), args=[expr], keywords=[])
+            if value.format_spec:
+                if isinstance(value.format_spec, ast.JoinedStr):
+                    spec = joinstr1(value.format_spec)
+                else:
+                    spec = value.format_spec
+                expr = Call(func=Attribute(value=expr, attr='__format__', ctx=Load()), args=[spec], keywords=[])
+            expr = IfExp(test=Constant(value=True), body=expr, orelse=expr)
+            parts.append(expr)
+        elif isinstance(value, ast.JoinedStr):
+            parts.append(joinstr1(value))
+        else:
+            parts.append(Call(func=Name(id='__AnhNguyen__', ctx=Load()), args=[value], keywords=[]))
+    if not parts:
+        return Constant(value='')
+    result = parts[0]
+    for p in parts[1:]:
+        result = BinOp(left=result, op=Add(), right=p)
+    return result
+
+class fstring1(ast.NodeTransformer):
+
+    def visit_JoinedStr(self, node):
+        self.generic_visit(node)
+        return joinstr1(node)
 
 def _args(name):
     return ast.arguments(posonlyargs=[], args=[ast.arg(arg=name)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[])
@@ -270,6 +385,18 @@ class junk(ast.NodeTransformer):
         node.body = new_body
         return node
 
+class RenameAll(ast.NodeTransformer):
+
+    def __init__(self):
+        if sys.gettrace() is not None:
+            os._exit(0)
+        self.map = {}
+        self.builtins = set(dir(builtins))
+        self.pool = [(lambda f: f((lambda: 20000 * 2 + 4096 + random.randint(0x2160, 0x217F))()))(getattr(__import__('builtins'), ''.join(map(chr, [99, 104, 114])))) for _ in range(15)]
+
+    def cak(self):
+        return (lambda j: j((random.choice(self.pool) for _ in range(3))))(''.join)
+        
 def encode(data: bytes):
     out = []
     for b in data:
@@ -324,37 +451,47 @@ except Exception as e:print(e)
 except KeyboardInterrupt:pass"""
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: python enc.py <file.py>')
+    if len(sys.argv) == 2:
+        print("Run: python enc.py <file>.py")
+        file = sys.argv[1]
+    else:
+        file = input("Enter File: ").strip()
+
+    if not file:
+        print("No file was specified.")
         return
-    file = sys.argv[1]
+
     if not os.path.exists(file):
-        print('File not found.')
+        print("File not found.")
         return
 
     with open(file, 'r', encoding='utf-8') as f:
-        code = ast.parse(anti+loadcode + f.read())
+        code = ast.parse(loadcode+anti + f.read())
 
-    print("f_string...")
-    tree = fstring().visit(code)
-    print("hide builtins")
-    tree = hide().visit(tree)
-    print("obfstring...")
+    print("Rename Vars...")
+    tree = RenameAll().visit(code)   
+    print("F-String...")
+    tree = fstring().visit(tree)
+    tree = fstring1().visit(tree)
+    print("Obf-String...")
     code = obfstring().visit(tree)
-    tree = speed(code)
     print("Adding Junkcode...")
-    code = junk().visit(tree)
+    code = junk().visit(code)
+    print("Obf-Junk...")
+    code = speed(code)
     ast.fix_missing_locations(code)
 
-    code = marshal.dumps(compile(ast.unparse(code), 'MiToMa', 'exec'))
+    code = marshal.dumps(compile(ast.parse(code), 'MiToMa', 'exec'))
     code = encode(code)
     loader = sanh(code)
 
-    out = "obf-" + file
+    out = "obf-" + os.path.basename(file)
 
     with open(out, 'w', encoding='utf-8') as f:
         f.write(loader)
-    print('Done ->', out)
 
+    print("Done ->", out)
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:pass
